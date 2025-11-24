b12dbf98de26ad3ec7df41d889bdcc5d16290b1f
Zehra0624 <zehraahmad147@gmail.com>
2025-11-24 22:59:55 +0500

    Bug Fix 1: Validating inputs

--- src/main.cpp
+++ src/main.cpp
@@ -2,18 +2,33 @@
 
 using namespace std;
 
-int main(){
+int main() {
     double loan_amount,interest_rate,number_of_years,total_amount,monthly_amount;
+    
+	//Big Fix 1: Valdating Inputs to prevent negative or invalid loan amounts
+    do {
+      cout << "Enter the loan amount: ";
+      cin >> loan_amount;
+      if(loan_amount <= 0) {
+        cout << "Error: Loan amount must be positive!" << endl;
+	  }
+    } while(loan_amount <= 0);
 
-	cout<<"Enter the loan amount: ";
-	cin>>loan_amount;
-	cout<<endl;
-	cout<<"Enter the interest rate: ";
-	cin>>interest_rate;
-	cout<<endl;
-	cout<<"The number of years: ";
-	cin>>number_of_years;
-	cout<<endl;
+    do {
+      cout << "Enter the interest rate: ";
+      cin >> interest_rate;
+      if(interest_rate < 0) {
+        cout << "Error: Interest rate cannot be negative!" << endl;
+	  }
+    } while(interest_rate < 0);
+
+    do {
+      cout << "The number of years: ";
+      cin >> number_of_years;
+      if(number_of_years <= 0) {
+        cout << "Error: Tenure must be at least 1 year!" << endl;
+	  }
+    } while(number_of_years <= 0);
 
 	total_amount=(number_of_years*loan_amount)+(number_of_years*loan_amount*(interest_rate/100.00));
 	monthly_amount=total_amount/(number_of_years*12);
