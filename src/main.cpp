b55ce8ef24314ead9d33dc2aa7a276d31bb73d47
Zehra0624 <zehraahmad147@gmail.com>
2025-11-24 23:19:40 +0500

    Bug Fix 2+3: using long double and step by step calculation to prevent overflow

--- src/main.cpp
+++ src/main.cpp
@@ -3,7 +3,8 @@
 using namespace std;
 
 int main() {
-    double loan_amount,interest_rate,number_of_years,total_amount,monthly_amount;
+    double loan_amount,interest_rate,number_of_years;
+	long double total_amount, monthly_amount; //Bug Fix 2: Safer calculations using long double
     
 	//Big Fix 1: Valdating Inputs to prevent negative or invalid loan amounts
     do {
@@ -29,9 +30,12 @@ int main() {
         cout << "Error: Tenure must be at least 1 year!" << endl;
 	  }
     } while(number_of_years <= 0);
-
-	total_amount=(number_of_years*loan_amount)+(number_of_years*loan_amount*(interest_rate/100.00));
-	monthly_amount=total_amount/(number_of_years*12);
+    
+	//Bug Fix 3: Calculations done step by step to prevent overflow 
+	long double principal = loan_amount; 
+    long double interest = principal * (interest_rate / 100.0L) * number_of_years; 
+    total_amount = principal * number_of_years + interest;             
+    monthly_amount = total_amount / (number_of_years * 12);            
 
 	cout<<"Total amount to be paid: "<<total_amount<<endl;
 	cout<<"Total interest: "<<total_amount-(number_of_years*loan_amount)<<endl;
