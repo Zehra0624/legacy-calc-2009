8ebda803ed172764a69044ac0afbe7b112f1e4de
Zehra0624 <zehraahmad147@gmail.com>
2025-11-25 18:03:37 +0500

    Adding Doxygen html document

---
+++ docs/html/search/functions_0.js
@@ -0,0 +1,6 @@
+var searchData=
+[
+  ['calculateinterest_0',['calculateInterest',['../class_loan.html#a0623f080fa715155087451749a0b87b9',1,'Loan']]],
+  ['calculatemonthlypayment_1',['calculateMonthlyPayment',['../class_loan.html#af5b1d81089c139a2aecb909ecd635876',1,'Loan']]],
+  ['calculatetotalamount_2',['calculateTotalAmount',['../class_loan.html#a3caef48df0aa1524b9e05ed0a72da1da',1,'Loan']]]
+];
---
+++ docs/html/search/functions_1.js
@@ -0,0 +1,4 @@
+var searchData=
+[
+  ['loan_0',['Loan',['../class_loan.html#a11f4a156ba1c3b5ac42182a4ea37b292',1,'Loan']]]
+];
