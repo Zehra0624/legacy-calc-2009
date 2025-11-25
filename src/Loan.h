1f321c6432619bf5180742ccfadd70bc98271c77
Zehra0624 <zehraahmad147@gmail.com>
2025-11-25 17:55:37 +0500

    Updating Loan.h

--- src/Loan.h
+++ src/Loan.h
@@ -1,17 +1,36 @@
-#ifndef LOAN_H
-#define LOAN_H
-
+/**
+ * @class Loan
+ * @brief Stores loan details and calculates total, interest and monthly payment.
+ */
 class Loan {
 public:
     long double loan_amount;
     long double interest_rate;
     long double number_of_years;
 
+    /**
+     * @brief Constructor to initialize loan values.
+     * @param la Loan amount
+     * @param ir Interest rate
+     * @param ny Number of years
+     */
     Loan(long double la = 0, long double ir = 0, long double ny = 0);
 
+    /**
+     * @brief Calculates total amount payable (principal + interest)
+     * @return long double Total amount to pay
+     */
     long double calculateTotalAmount() const;
+
+    /**
+     * @brief Calculates monthly payment (EMI)
+     * @return long double Monthly installment
+     */
     long double calculateMonthlyPayment() const;
+
+    /**
+     * @brief Calculates total interest payable
+     * @return long double Total interest
+     */
     long double calculateInterest() const;
 };
-
-#endif
